10c651e7149b6ee40b587a6985150712e8ff784e
walidelmoumen <walidelmoumen2002@gmail.com>
2024-12-10 21:40:08 +0100

    fix

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -10,8 +10,8 @@ const Contact = () => {
     return (
         <div id="contact" className="text-[#f1f0e4] flex flex-col md:flex-row items-center justify-around py-16 md:py-32">
             <div className='font-montserrat'>
-                <h2 className="font-bold text-4xl md:text-5xl text-[#156782] text-center pb-10 md:text-start" >Get In Touch</h2>
-                <div className="flex w-full flex-col font-montserrat mb-10 md:p-0 md:m-0">
+                <h2 className="font-bold text-4xl md:text-5xl text-[#156782] text-center md:pb-10 md:text-start" >Get In Touch</h2>
+                <div className="flex  p-10 flex-col font-montserrat mb-10 md:p-0 md:m-0">
                     <a href='tel:+212638-683227' className=" bg-[#156782] rounded-md hover:shadow-xl flex flex-row items-center h-20  md:text-left px-5 md:px-10 py-3 ">
                         <FaPhoneVolume className='text-4xl mr-4' />
                         <div>
