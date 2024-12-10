9af93281d10f637f290ccb6618f1c7e89bd9c30c
walidelmoumen <walidelmoumen2002@gmail.com>
2024-12-10 21:34:29 +0100

    add adress

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import instagram from '../assets/instagram.svg'
-import { FaPhoneVolume } from "react-icons/fa";
+import { FaPhoneVolume, FaMapMarkerAlt } from "react-icons/fa";
 import { IoIosMail } from "react-icons/io";
 import logo from '../assets/logonour.png'
 import contact from '../assets/contact.png'
 import { Link } from "react-scroll";
-
 
 const Contact = () => {
     return (
@@ -13,22 +12,32 @@ const Contact = () => {
             <div className='font-montserrat'>
                 <h2 className="font-bold text-4xl md:text-5xl text-[#156782] text-center pb-10 md:text-start" >Get In Touch</h2>
                 <div className="flex w-full flex-col font-montserrat mb-10 md:p-0 md:m-0">
-                    <div className=" bg-[#156782] rounded-md hover:shadow-xl flex flex-row items-center h-20  md:text-left px-5 md:px-10 py-3 ">
+                    <a href='tel:+212638-683227' className=" bg-[#156782] rounded-md hover:shadow-xl flex flex-row items-center h-20  md:text-left px-5 md:px-10 py-3 ">
                         <FaPhoneVolume className='text-4xl mr-4' />
                         <div>
                             <h5 className='text-xl font-bold'>Phone:</h5>
                             <p className='text-sm font-normal'>+212 638-683227</p>
                         </div>
-                    </div>
+                    </a>
                     <div className="divider "></div>
 
-                    <div className=" bg-[#156782] rounded-md hover:shadow-xl flex flex-row items-center h-20  text-left px-5 md:px-10 py-3 ">
+                    <button onClick={() => window.location = 'mailto:noureddinelassaoui81@gmail.com'} className=" bg-[#156782] rounded-md hover:shadow-xl flex flex-row items-center h-20  text-left px-5 md:px-10 py-3 ">
                         <IoIosMail className='text-4xl mr-4' />
                         <div >
                             <h5 className='text-xl font-bold'>Mail:</h5>
                             <p className='text-sm font-normal'>noureddinelassaoui81@gmail.com</p>
                         </div>
-                    </div>
+                    </button>
+
+                    <div className="divider "></div>
+
+                    <a href='https://maps.app.goo.gl/RQzyemfGhYpBMF5T7?g_st=ic' className=" bg-[#156782] rounded-md hover:shadow-xl flex flex-row items-center h-20  md:text-left px-5 md:px-10 py-3 ">
+                        <FaMapMarkerAlt className='text-4xl mr-4' />
+                        <div>
+                            <h5 className='text-xl font-bold'>Address:</h5>
+                            <p className='text-sm font-normal'>AV. Moulay Abdelkader, quartier Al-Inbaat, n° 239</p>
+                        </div>
+                    </a>
                 </div>
             </div>
             <div>
